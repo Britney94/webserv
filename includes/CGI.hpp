0f9282a934d6f3687f6dca5cda4e9b00e4b35a3d
shell02 <92311244+shell02@users.noreply.github.com>
2023-01-17 09:59:00 +0100

    Update CGI.hpp

--- includes/CGI.hpp
+++ includes/CGI.hpp
@@ -1,9 +1,20 @@
 #ifndef CGI_HPP
-# define CGI_HPP
+#define CGI_HPP
 
 #include "webserv.hpp"
 
-class	CGI {
+class CGI{
+	public:
+		CGI();
+		CGI(const CGI &src);
+		CGI &operator=(const CGI &src);
+		std::string	execute(const std::string& scriptName);
+		~CGI(){};
+	private:
+		std::string	_body;
+		std::map<std::string, std::string>	_env;
+		void	_setEnv();
+		char	**_convertEnv() const;
 };
 
 #endif
